bf738db039484239da5f904fcbdd7a39c7a2a3a3
SanjuTech1 <sanjusajeev055@gmail.com>
2025-02-05 15:22:46 +0530

    Added Seperate Nav for Home

---
+++ client/src/components/HomeNav.jsx
@@ -0,0 +1,28 @@
+import React, { useContext } from "react";
+import { assets } from "../assets/assets";
+import { useNavigate } from "react-router-dom";
+import { AppContent } from "../context/AppContext";
+
+const HomeNav = () => {
+  const navigate = useNavigate();
+  const { userData } = useContext(AppContent);
+
+  return (
+    <div className="w-full flex justify-between items-center p-4 sm:p-6 sm:px-24 absolute top-0">
+      <div>
+        <img src={assets} alt="" className="w-28 sm:w-32" />
+      </div>
+
+      {!userData && (
+        <button
+          onClick={() => navigate("/login")}
+          className="flex items-center gap-2 border border-gray-500 rounded-full px-6 py-2 text-gray-800 hover:bg-gray-100"
+        >
+          Login <img src={assets.arrow_icon} alt="" />
+        </button>
+      )}
+    </div>
+  );
+};
+
+export default HomeNav;
