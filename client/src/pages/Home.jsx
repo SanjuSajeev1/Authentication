fffeef6080e082468d748ea9a06122beb4f9ef2f
SanjuTech1 <sanjusajeev055@gmail.com>
2025-02-05 15:24:08 +0530

    called Home Nav

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,11 +1,12 @@
 import React from "react";
-import Navbar from "../components/Navbar";
+
 import Header from "../components/Header";
+import HomeNav from "../components/HomeNav";
 
 const Home = () => {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen bg-[url('/bg_img.png')] bg-cover bg-center">
-      <Navbar />
+      <HomeNav />
       <Header />
     </div>
   );
